62d3a841833a0b1f531e98c2d792d550bb4ce50d
Mr.Mao <951416545@qq.com>
2024-07-27 12:24:02 +0800

    chore: update ensure log and logical order

--- lib/index.ts
+++ lib/index.ts
@@ -7,38 +7,40 @@ import { readSelfReferenced } from './read-self-referenced'
 import { caller } from './caller'
 
 
+export function resolveImport(importName: string, ensure?: true, resolveOptions?: resolve.SyncOpts): string
+export function resolveImport(importName: string, ensure?: false, resolveOptions?: resolve.SyncOpts): string | undefined
 export function resolveImport(
   importName: string,
   ensure = false,
   resolveOptions: resolve.SyncOpts = { preserveSymlinks: false }
 ) {
   const basedir = resolveOptions.basedir || path.dirname(caller())
+  let tryError
   try {
     return resolve.sync(importName, { ...resolveOptions, basedir })
   }
-  catch (error) {
-    const selfPackagePath = readSelfReferenced(importName, basedir)
-    if (selfPackagePath)
-      return selfPackagePath
-    else
-      consola.log(error)
-  }
+  catch (error) { tryError = error }
 
   if (isInstalledGlobally) {
     try {
       return require.resolve(join(globalDirs.yarn.packages, importName))
     }
-    catch { }
+    catch (error){ tryError = error }
 
     try {
       return require.resolve(join(globalDirs.npm.packages, importName))
     }
-    catch { }
+    catch (error){ tryError = error }
   }
 
-  if (ensure)
-    throw new Error(`Failed to resolve package ${importName}`)
+  const selfPackagePath = readSelfReferenced(importName, basedir)
+  if (selfPackagePath)
+    return selfPackagePath
 
-  consola.error(`Failed to resolve package ${importName}`)
+  if (ensure) {
+    consola.log(tryError)
+    throw new Error(`Failed to resolve package ${importName}`)
+  }
+
   return undefined
 }
